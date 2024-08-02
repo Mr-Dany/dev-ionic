71a39fd2d61ff91c04a670aa1ad53523da7717d7
Mr-Dany <49878464+Mr-Dany@users.noreply.github.com>
2024-08-01 21:28:40 -0500

    Configuracion de Firebase

--- product-admin-ionic/src/environments/environment.prod.ts
+++ product-admin-ionic/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCwIX18SVejh20Gk7Us5tmld4zM-1Vwo2o",
+    authDomain: "product-admin-app-d218d.firebaseapp.com",
+    projectId: "product-admin-app-d218d",
+    storageBucket: "product-admin-app-d218d.appspot.com",
+    messagingSenderId: "719685855904",
+    appId: "1:719685855904:web:f99d7664aa19bcb2adb7c5"
+  }
 };
--- product-admin-ionic/src/environments/environment.ts
+++ product-admin-ionic/src/environments/environment.ts
@@ -3,8 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCwIX18SVejh20Gk7Us5tmld4zM-1Vwo2o",
+    authDomain: "product-admin-app-d218d.firebaseapp.com",
+    projectId: "product-admin-app-d218d",
+    storageBucket: "product-admin-app-d218d.appspot.com",
+    messagingSenderId: "719685855904",
+    appId: "1:719685855904:web:f99d7664aa19bcb2adb7c5"
+  }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
